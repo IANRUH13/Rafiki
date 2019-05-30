3b0b80e7b7409cf3da307c4f77728252d5b00b82
bmugenya <bmugenya26@gmail.com>
2019-05-30 09:29:05 +0300

    [Feature #166336689] added database connection

---
+++ db_con.py
@@ -0,0 +1,28 @@
+import psycopg2
+
+url = "dbname='rafiki' user='postgres' host='localhost' port=5432 password='Boywonder47'"
+
+
+class database_setup(object):
+
+    def __init__(self):
+        self.conn = psycopg2.connect(url)
+        self.cursor = self.conn.cursor()
+
+    def destroy_tables(self):
+        self.cursor.execute("""DROP TABLE IF EXISTS user CASCADE;""")
+
+        self.conn.commit()
+
+    def create_tables(self):
+        self.cursor.execute("""CREATE TABLE IF NOT EXISTS Users (
+            name VARCHAR(50) NOT NULL,
+            post_date TIMESTAMP,
+            email VARCHAR(50)  UNIQUE NOT NULL,
+            password VARCHAR(255) NOT NULL,
+            photo VARCHAR(255) NOT NULL,
+            PRIMARY KEY (email)
+            );""")
+
+
+        self.conn.commit()
